53996909a2b860d8e04016adc3af52db948b69eb
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2022-11-09 12:11:52 -0500

    commit tree 4f4f99a4268eed7de2983a4539ceaacfdf0c0d6d

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,9 @@
 class UsersController < ApplicationController
+    
+    # GET /users/:id
+    def show 
+        users = User.find_by(id: params[:id]) 
+        render json: users, include: :items
+    end
+    
 end
